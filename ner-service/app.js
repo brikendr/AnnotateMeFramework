05abcda6ebb860014327b934fe27eb83693d700d
Brikend Rama <brikend.rama@gmail.com>
2017-03-03 11:11:20 +0100

    Removing unecessary dependencies and public folder from the ner-service project structure.

--- ner-service/app.js
+++ ner-service/app.js
@@ -1,6 +1,4 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -9,17 +7,11 @@ var entity_extractor = require('./routes/entity_extractor');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/', entity_extractor);
